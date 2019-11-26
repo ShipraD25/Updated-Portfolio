084f519903af51a2005c7c289683bf5b4318060d
Shipra <drshipradyundi@gmail.com>
2019-11-25 19:15:14 -0800

    fixed a error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
     $("#contact-form").submit(function(event) {
+        event.preventDefault();
         var name = $("#ExampleInputname").val();
         var email = $("#ExampleInputemail").val();
-        var message = $("exampleFormControlTextarea1").val();
-
+        var message = $("#exampleFormControlTextarea1").val();
+        console.log(name, email, message);
         if (!name.value || !email.value || !message.value) {
-            alert.error("Please fill the form");
+            // alert.error("Please fill the form");
             return false;
         } else {
             return true;
-            event.preventDefault();
-            sendmail();
+            // window.location.href = "mailto:user@example.com?subject=Subject&body=message%20goes%20here"
+            window.location.href = "mailto:drshipradyundi@gmail.com?" + "subject=" + name + "&body=" + message;
+            //sendmail();
             //FormData.reset();
         }
     });
 
-    function sendMail() {
-        var link = "mailto:drshipradyundi@gmail.com?" + "&subject=" + name + "&body=" + message;;
-    }
+    //function sendMail() {
+    //  var link = "mailto:drshipradyundi@gmail.com?" + "&subject=" + name + "&body=" + message;;
+    // }
 });
